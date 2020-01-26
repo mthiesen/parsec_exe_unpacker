3637f3e0bd0876391df04ebb10cbf4f048c17e70
Malte Thiesen <malte@kamalook.de>
2020-01-26 08:53:23 +0100

    Updated to unicorn-rs 0.9.1

--- src/unpacker.rs
+++ src/unpacker.rs
@@ -36,10 +36,10 @@ pub fn emulate_unpacker(
     base_segment: u16,
     header_info: &Info,
 ) -> Result<UnpackedExecutable> {
-    let mut emulator = CpuX86::new(Mode::MODE_16)
+    let emulator = CpuX86::new(Mode::MODE_16)
         .chain_err_msg("Failed to create 16-bit x86 Unicorn emulator.")?;
     emulator
-        .mem_map(0, 0xa0000, unicorn::PROT_ALL)
+        .mem_map(0, 0xa0000, unicorn::Protection::ALL)
         .chain_err_msg("Failed to map emulator memory.")?;
 
     assert!(base_segment >= 0x10);
@@ -110,8 +110,9 @@ pub fn emulate_unpacker(
         .reg_read(RegisterX86::SP)
         .chain_err_msg("Failed to read emulators SP register.")? as u16;
 
-    let unpacked_data = emulator
-        .mem_read(load_location, header_info.total_alloc_len)
+    let mut unpacked_data = vec![0u8; header_info.total_alloc_len];
+    emulator
+        .mem_read(load_location, &mut unpacked_data)
         .chain_err_msg("Failed to read unpacked executable data from emulator.")?;
 
     Ok(UnpackedExecutable {
